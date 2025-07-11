55727d2ce32698fdc9d2037fcbc75c2331f3f597
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2025-07-11 10:32:18 +0545

    Update third umpire.py

--- third umpire using python/third umpire.py
+++ third umpire using python/third umpire.py
@@ -1,31 +1,4 @@
-import tkinter 
-import cv2 
-import PIL.Image, PIL.ImageTk 
-from functools import partial 
-import threading
-import time
-import imutils 
 
-stream = cv2.VideoCapture("clip.mp4")
-flag = True
-
-def play(speed):
-    global flag
-    print(f"You clicked on play. Speed is {speed}")
-
-    frame1 = stream.get(cv2.CAP_PROP_POS_FRAMES)
-    stream.set(cv2.CAP_PROP_POS_FRAMES, frame1 + speed)
-
-    grabbed, frame = stream.read()
-    if not grabbed:
-        exit()
-    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
-    frame = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame).resize((SET_WIDTH, SET_HEIGHT)))
-    canvas.image = frame
-    canvas.create_image(0, 0, image=frame, anchor=tkinter.NW)
-    if flag:
-        canvas.create_text(134, 26, fill="white", font="Times 26 bold", text="Decision Pending")
-    flag = not flag
 
 def pending(decision):
     frame = cv2.cvtColor(cv2.imread("pending.jpg"), cv2.COLOR_BGR2RGB)
